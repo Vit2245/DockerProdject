35d784afb720c5843bc7269d19489f7c098df97a
Vit2245 <Vitas930831@mail.ru>
2023-11-07 23:48:13 +0300

    Create Docker project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate_random_number', methods=['POST'])
+def generate_random_number():
+    if request.method == 'POST':
+        random_number = random.randint(1, 100)
+        return str(random_number)
+
+if __name__ == '__main__':
+    app.run(debug=True)
